9e2722ee71353b696edace41e13aa66f9c657e42
konkolyikrisztina <konkolyikrisztina@gmail.com>
2016-12-14 00:13:35 +0100

    Doxygen comments added.

--- app/RoboControll/robothandler.h
+++ app/RoboControll/robothandler.h
@@ -10,45 +10,158 @@
 
 #include <QTcpSocket>
 
-
+/**@brief The RobotHandler is responsible for the communication between the robot and the UI.
+ *
+ * The class receives and sends commands from/to the UI with the help of the signals and slots mechanism.
+ * It can be connected either to a robot or an emulated robot (which can be controlled through the UI).
+*/
 class RobotHandler : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief RobotHandler constructor.
+     * @param parent A pointer to the parent, if the instance has a parent object.
+     * @note It is recommended to specify a parent object in order to call the RobotHandler's destructor automatically on destruction of the parent.
+     */
     explicit RobotHandler(QObject *parent = 0);
 
 signals:
+    /**
+     * @brief Notifies the UI of a change in the connection status.
+     */
     void connectionStatusChanged(bool,bool);
+
+    /**
+     * @brief Signals the arrival of a new message to be written to the log on the UI.
+     * @note It is recommended to connect this signal to the UI.
+     * @param msg Any message containing human readable text/information.
+     */
     void logLine(QString msg);
+
+    /**
+     * @brief Tells the UI to clear the log.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void logClear();
+
+    /**
+     * @brief Notifies the UI about the changed touch sensor value and supplies the new value.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void touchSensorChanged(bool);
+
+    /**
+     * @brief Notifies the UI about the changed speed and supplies the new value.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void speedChanged(QString);
+
+    /**
+     * @brief Notifies the UI about the changed steer and supplies the new value.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void steerChanged(QString);
+
+    /**
+     * @brief Notifies the UI about the changed distance value and supplies the new value.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void distanceChanged(qreal);
+
+    /**
+     * @brief Notifies the UI about the changed RGB sensor value and supplies the new R, G and B color values.
+     * @note It is recommended to connect this signal to the UI.
+     */
     void rgbChanged(qreal,qreal,qreal);
 
 public slots:
+    /**
+     * @brief Validates the received address, connects tcpSocket to the specified IP and port and emits connectionStatusChanged signal.
+     * @param address The address in <IP>:<port> format.
+     */
     void onConnect(QString address);
+
+    /**
+     * @brief Disconnects tcpSocket from the host and emits connectionStatusChanged signal.
+     */
     void onDisconnect();
+
+    /**
+     * @brief Sends command to the robot or (in emulation mode) takes it as if it were sent by the robot 
+     * @param command Includes the verb and optionally parameter(s) separated by spaces (" ").
+     * @param isEmulatedInput indicates emulation mode
+     */
     void onSend(QString command,bool isEmulatedInput);
 
 private slots:
+    /**
+     * @brief Reads any data from tcpSocket.
+     */
     void readTcpData();
+
+    /**
+     * @brief Informs the UI and the user of the successful connection by emiting the signals connectionStatusChanged and logLine.
+     */
     void onTcpConnected();
+
+    /**
+     * @brief Informs the UI and the user of the disconnection by emiting the signals connectionStatusChanged and logLine.
+     */
     void onTcpDisconnected();
+
+    /**
+     * @brief Notifies the UI and the user of the error by emiting the signals connectionStatusChanged and logLine.
+     */
     void onTcpError();
+
+    /**
+     * @brief Determines the proper command and sends it to the robot.
+     * @param speed Specifies the speed value [m/s] to be sent to the robot.
+     * @param steer Specifies the steering angle to be sent to the robot.
+     * @param isForward If true, the robot will be directed to move forward.
+     * @param isBackward If true, the robot will be directed to move backward.
+     * @param isStop If true, the "stop" command is sent.
+     */
     void speedAndSteerChanged(qreal speed,qreal steer,bool isForward,bool isBackward,bool isStop);
+
+    /**
+     * @brief Sends the command "steertest" to the robot.
+     */
     void steerTest();
+
+    /**
+     * @brief Sends the command "starttest" to the robot.
+     */
     void startTest();
 
 
 private:
+    /**
+     * @brief Client TCP Socket.
+     */
     QTcpSocket *tcpSocket;
+
+    /**
+     * @brief Splits command into verb and parameters.
+     * @param command Command including a verb and optionally parameter(s) separated by space (" ") characters.
+     * @return Returns a pair which consists of the verb and the parameter list.
+     */
     static std::pair<QString,QStringList> preprocessCommand(QString command);
+
+    /**
+     * @brief Interprets the command specified by the verb and the parameters.
+     * @param verb Representing a valid command.
+     * @warning Allowed verbs are: ping, touch, speed, steer, distance, rgb, startresult, steerresult, log
+     * @param params Depending on the command it can be empty or can contain parameter values.
+     */
     void executeCommand(QString verb,QStringList params);
+
+    /**
+     * @brief Sends command to robot if the device (tcpSocket) is open, i.e. can be written to. Outputs command to logLine.
+     * @param command The command (verb and parameters) received from the UI.
+     */
     void sendCommandToRobot(QString command);
-
-
 };
 
 #endif // ROBOTHANDLER_H
